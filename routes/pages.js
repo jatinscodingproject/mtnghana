4f78d960c52744b1764253bcf1a4b0db8c2e1f0c
jatinscodingproject <jatincoder104@gmail.com>
2026-07-21 20:16:39 +0530

    code update

--- routes/pages.js
+++ routes/pages.js
@@ -5,11 +5,3 @@ const authMiddleware = require("../middleware/middleware.authMiddleware");
 
 router.get("/", PagesController.homePage);
 router.get("/login", PagesController.loginPage);
-router.get("/register", PagesController.registrationPage);
-router.get("/privacy", PagesController.privacyPage)
-router.get("/terms", PagesController.termsPage)
-router.get("/faq", PagesController.faqPage)
-router.get("/verify", PagesController.verifyPage)
-router.get("/new-verify", PagesController.newVerifyPage)
-router.get("/my-account", authMiddleware, PagesController.accountPage)
-module.exports = router;
